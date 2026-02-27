4e0bff18a19d73a421d286f3625bfc67bdc69e97
Alejandro <ale.torrubiano@gmail.com>
2026-02-27 23:30:34 +0100

    chore: shadcn installation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', 'shadcn-nuxt'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   vite: {
